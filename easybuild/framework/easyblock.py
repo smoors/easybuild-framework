3528e89ca70dcb6f7346f4a9003d1ba4e1261d9f
Samuel Moors <samuel.moors@vub.be>
2021-06-11 09:32:34 +0200

    first part

--- easybuild/framework/easyblock.py
+++ easybuild/framework/easyblock.py
@@ -443,7 +443,7 @@ class EasyBlock(object):
         for index, patch_spec in enumerate(patch_specs):
 
             # check if the patches can be located
-            copy_file = False
+            just_copy_file = False
             suff = None
             level = None
             if isinstance(patch_spec, (list, tuple)):
@@ -459,12 +459,14 @@ class EasyBlock(object):
                 elif isinstance(patch_spec[1], string_type):
                     # non-patch files are assumed to be files to copy
                     if not patch_spec[0].endswith('.patch'):
-                        copy_file = True
+                        just_copy_file = True
                     suff = patch_spec[1]
                 else:
                     raise EasyBuildError("Wrong patch spec '%s', only int/string are supported as 2nd element",
                                          str(patch_spec))
             else:
+                if not patch_file.endswith('.patch'):
+                    just_copy_file = True
                 patch_file = patch_spec
 
             force_download = build_option('force_download') in [FORCE_DOWNLOAD_ALL, FORCE_DOWNLOAD_PATCHES]
@@ -477,8 +479,10 @@ class EasyBlock(object):
                     'checksum': self.get_checksum_for(checksums, index=index),
                 }
                 if suff:
-                    if copy_file:
+                    if just_copy_file:
                         patchspec['copy'] = suff
+                        self.log.debug('Patch %s does not have ".patchc" suffix, assuming regular file.'
+                                       'Just copying, not patching', patch_spec)
                     else:
                         patchspec['sourcepath'] = suff
                 if level is not None:
