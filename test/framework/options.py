58e859b89b1b7c67bd158feaccf1f3ca3b8a0b93
Samuel Moors <samuel.moors@vub.be>
2024-05-22 21:32:03 +0200

    fix bug

--- test/framework/options.py
+++ test/framework/options.py
@@ -366,8 +366,8 @@ class CommandLineOptionsTest(EnhancedTestCase):
 
         self.assertEqual(len(glob.glob(toy_mod_glob)), 1)
 
-        # check use of module_only + skip sanity check to generate a valid module
-        test_ec_txt += "\nskipsteps = ['module_only']\n"
+        # check use of module_only parameter + skip sanity check to generate a valid module
+        test_ec_txt += "\nmodule_only = True\n"
         write_file(test_ec, test_ec_txt)
         self.eb_main(args, do_build=True, raise_error=True)
 
